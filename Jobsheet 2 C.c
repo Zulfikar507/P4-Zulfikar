12016b0cb09a9e1e28d51985f2ab5f3a1c9b815b
Zul Fikar <64571549+Zulfikar507@users.noreply.github.com>
2020-05-12 14:51:30 +0700

    Create Jobsheet 2 C.c
    Buatlah Program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap mahasiswa dalam bahasa C

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+main()
+{
+	char* x [8];
+printf("NAMA : ZULFIKAR \nNIM :F1B019147 \nKELOMPOK : 28 \n\n\n");
+printf("masukkan nim mahasiswa pertama = ");
+scanf("%s",&x[0]);
+printf("masukkan nim mahasiswa kedua=");
+scanf("%s",&x[1]);
+printf("masukkan nim mahasiswa ketiga = ");
+scanf("%s", &x[2]);
+printf("Masukan nim mahasiswa keempat = ");
+scanf("%s",&x[3]);
+printf("Masukan nim mahasiswa kelima = ");
+scanf("%s",&x[4]);
+printf("masukkan nim mahasiswa keenam = ");
+scanf("%s",&x[5]);
+printf("masukkan nim mahasiswa ketujuh = ");
+scanf("%s",&x[6]);
+printf("masukkan nim mahasiswa kedelapan = ");
+scanf("%s",&x[7]);
+printf("masukkan nim mahasiswa kesembilan = ");
+scanf("%s",&x[8]);
+}
+
