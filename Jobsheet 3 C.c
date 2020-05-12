5df7baf418007869db58baa088686faec46e69b7
Zul Fikar <64571549+Zulfikar507@users.noreply.github.com>
2020-05-12 14:53:17 +0700

    Create Jobsheet 3 C.c
    Buatlah program penjumlahan matriks dua dimensi dengan memasukan NIM dua orabg mahasiswa dalam bahsa C

---
+++ Jobsheet 3 C.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+int i,j,matrik1[2][2],matrik2[2][2],jumlah[2][2];
+printf("NAMA : ZULFIKAR \nNIM : F1B019147 \nKELOMPOK : 28\n\n\n");
+for(i=0;i<2;i++){
+for(j=0;j<2;j++){
+printf("masukkan nilai matrik1  %i , %i",i,j);
+scanf("%i",&matrik1[i][j]);
+}
+printf("\n");
+}
+for(i=0;i<2;i++){
+for(j=0;j<2;j++){
+printf("masukkan nilai matrik2  %i , %i",i,j);
+scanf("%i",&matrik2[i][j]);
+}
+printf("\n");
+}
+for(i=0;i<2;i++){
+for(j=0;j<2;j++){
+ jumlah[i][j]=matrik1[i][j]+matrik2[i][j];
+printf("%i",jumlah[i][j]);
+}
+}
+}
+
