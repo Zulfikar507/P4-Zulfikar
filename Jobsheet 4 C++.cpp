5cb818f1064bb714fb120139379b4123779e4b7f
Zul Fikar <64571549+Zulfikar507@users.noreply.github.com>
2020-05-12 14:55:23 +0700

    Create Jobsheet 4 C++.cpp
    Buatlah program pengurangan matriks dua dimensi dengan menggunakan NIM dua orang mahasiswa dalam bahasa C++

---
+++ Jobsheet 4 C++.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+main ()
+{
+int i, j;
+int matrik1[2][2] ,matrik2[2][2] ,jumlah [2][2];
+cout << "Nama : ZULFIKAR" << endl;
+cout << "NIM : F1B019147" << endl;
+cout << "Kelompok : 28" << endl;
+for (i = 0; i < 2; i++)
+{
+for (j=0; j<2; j++)
+{
+cout << "Masukkan nilai matrik 1 = " << i << "," << j << " = ";
+cin >> matrik1[i][j];
+}
+}
+cout << endl;
+for (i=0; i< 2; i++)
+{
+for (j = 0; j < 2; j++)
+{
+cout << "Masukkan nilai matrik 2 = " <<i<< "," <<j<< " = ";
+cin >> matrik2[i][j];
+}
+}
+cout << endl;
+for (i=0; i< 2; i++){
+for (j = 0; j < 2; j++){
+jumlah[i][j] = matrik1[i][j]-matrik2[i][j];
+cout << jumlah [i][j] <<" ";
+}
+cout << endl;
+}
+}
+
