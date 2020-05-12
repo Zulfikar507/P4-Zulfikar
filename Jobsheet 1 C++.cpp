73a4559b1cc92559a7e06368ba3094ea8196e49a
Zul Fikar <64571549+Zulfikar507@users.noreply.github.com>
2020-05-12 14:50:19 +0700

    Create Jobsheet 1 C++.cpp
    Buatlah program array satu dimensi yang menampilkan NIM masing-masing mahasiswa dalam bahasa C++

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+    string my_array[7] = {"F1B018023","F1B018042","F1B01843","F1B018044","F1B018040","F1B018075","F1B019147"};
+    cout << "Nama : ZULFIKAR" << endl;
+    cout << "NIM : F1B019147" << endl;
+    cout << "Kelompok : 28" << endl;
+cout<<"\nData ke- 1 = "<<my_array[0]<<"\n";
+cout<<"Data ke- 2 = "<<my_array[1]<<"\n";
+cout<<"Data ke- 3 = "<<my_array[2]<<"\n";
+cout<<"Data ke- 4 = "<<my_array[3]<<"\n";
+cout<<"Data ke- 5 = "<<my_array[4]<<"\n";
+cout<<"Data ke- 6 = "<<my_array[5]<<"\n";
+cout<<"Data ke- 7 = "<<my_array[6]<<"\n";
+}
+
